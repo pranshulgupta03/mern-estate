cfe90a5d3f1bf4c9691cab91b4d74f1b7ce03bed
pranshulgupta03 <guptapranshu0801@gmail.com>
2024-03-06 13:52:38 +0530

    creating file and folder structure

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListingItem = () => {
+  return (
+    <div>
+    ListingItem
+    
+    </div>
+  )
+}
+
+export default ListingItem
---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OAuth = () => {
+  return (
+    <div>
+    OAuth
+    
+    </div>
+  )
+}
+
+export default OAuth
